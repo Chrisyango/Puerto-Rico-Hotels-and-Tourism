3c52b043850fec5e0d665557b1037cfc4ba1bf13
Chris Yang <cyang287@yahoo.com>
2019-02-27 11:48:38 -0500

    finished flyout and breadcrumbs

--- freeform.php
+++ freeform.php
@@ -1,68 +1,242 @@
 <!DOCTYPE html>
-	<html lang="en"> 
-		<head>
+<html lang="en">
+<head>
 
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-			<title>Site Name</title>
-			<meta name="description" content="">
-			<meta name="robots" content="index, follow">
+	<title>Puerto Rico, Hotels and Tourism - Freeform</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
 
-			<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">	
-			<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
-			<link rel="stylesheet" href="_assets_/fonts/Icon-font/pe-icon-7-stroke/css/pe-icon-7-stroke.css">	
-			<link rel="stylesheet" href="_assets_/css/animate.min.css">
-			<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">			
-			<link rel="stylesheet" href="_assets_/css/layout.css">
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
 
-			<link rel="shortcut icon" href="_assets_/images/favicon.ico">
-			<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
-			<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
-			<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
-			<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
 
-			<!--[if !IE]><!-->
-		 	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
-			<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>	
-			<script>addToHomescreen();</script>
-			<!--<![endif]-->
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 
-			<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
-			<!--[if lt IE 9]>
-			  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-			<![endif]-->
+</head>
+<body id="freeform">
 
-		</head>
-		<body id="freeform">
-		
-			<div class="main-wrap">	
-				<!-- Content Here -->
-				<!-- NOTE: Test Flyout with a tags and spans just like the main nav. Cannot assume all items are links. -->
-			</div><!-- /.main-wrap -->  
+<header class="clearfix">
+	<div class="container">
+		<div id="toggles" class="hidden-lg hidden-md">
+			<label for="search-input" id="search-toggle-mobile" class="fa fa-search"></label>
+			<div id="nav-toggle" class="fa fa-bars"></div>
+		</div><!-- /#toggles -->
+
+		<a href="./" class="logo">
+			<img src="./_assets_/images/logo.jpg" alt="Logo">
+		</a>
+
+		<div class="right-nav">
+		<div id="google-translate" class="hidden">
+			<script type="text/javascript">
+				function googleTranslateElementInit() {
+					new google.translate.TranslateElement({pagelanguage: 'en',
+						layout: google.translate.TranslateElement.InlineLayout.SIMPLE},
+						'google-translate');
+				}
+			</script>
+			<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+			</div><!--/#google-translate.control.hidden-sm.hidden-xs-->
+			<ul id="translation-links" class="hidden-sm hidden-xs">
+				<li><a href="#" class="Spanish" data-lang="Spanish">es</a></li>
+				<li><a href="#" class="English" data-lang="English">en</a></li>
+			</ul><!--/#translation-links-->
+			<div class="fa fa-search" id="search-toggle"></div>
+		</div><!-- /.right-nav -->
+
+		<div id="search">
+			<form class="search-form" method="get" action="search.php">
+				<label class="fa fa-search" for="search-input"></label>
+				<input name="q" class="form-control search-input" placeholder="I'm looking for..." type="search" id="search-input">
+				<button>Go</button>
+			</form><!-- /.search-form -->
+		</div><!-- /#search -->
+
+		<nav>
+			<ul id="nav">
+				<li><a href="./">home page</a></li>
+				<li><a href="./">about us</a></li>
+				<li><a href="./">member</a></li>
+				<li><a href="./">publications</a></li>
+			</ul> <!-- /#nav -->
+		</nav>
+	</div>
+</header>
+
+<section id="slider">
+	<ul class="bxslider">
+		<li style="background: url('./_assets_/images/hero1.jpg') top left no-repeat;background-size: cover;"></li>
+		<li style="background: url('./_assets_/images/inner-hero1.jpg') top left no-repeat;background-size: cover;"></li>
+		<li style="background: url('./_assets_/images/inner-hero2.jpg') top left no-repeat;background-size: cover;"></li>
+		<li style="background: url('./_assets_/images/inner-hero3.jpg') top left no-repeat;background-size: cover;"></li>
+		<li style="background: url('./_assets_/images/inner-hero4.jpg') top left no-repeat;background-size: cover;"></li>
+	</ul><!-- /.bxslider -->
+</section><!-- /#slider -->
+
+<main id="main">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<aside id="side-content">
+					<div id="flyout-wrap">
+						<ul id="flyout">
+							<span id="flyout-header" class="header">related pages</span>
+							<li><a href="./">bibendum fermentum ultricies euismod</a></li>
+							<li><a href="./">amet commodo sit</a></li>
+							<li>
+								<a href="./">euismod inceptos</a>
+								<ul>
+									<li><a href="./">flyout link</a></li>
+									<li><a href="./">flyout link</a></li>
+									<li><a href="./">flyout link</a></li>
+								</ul>
+							</li>
+							<li><a href="./">sollicitudin venenatis</a></li>
+							<li><a href="./">page title here lorem ipsum</a></li>
+							<li><a href="./">inceptos sit nullam pharetra</a></li>
+							<li><a href="./">amet malesuada euismod parturient ligula</a></li>
+						</ul><!-- /#flyout -->
+					</div><!-- /#flyout-wrap -->
+				</aside><!-- /#side-content -->
+			</div><!-- col-md-4 -->
+			<div class="col-md-8">
+				<article id="entry">
+					<section id="breadcrumbs">
+							<p>
+								<a href="./">
+									home
+								</a>
+								<a href="./">
+									section name
+								</a>
+								page title here lorem
+							</p>
+					</section><!-- /#breadcrumbs -->
+					<h1 id="page-title">adipiscing egestas tristique</h1>
+					<div id="post">
+						<p>
+							Represents the shared interests of its more than 450 corporate members, including hotels, restaurants, tour companies, suppliers of hospitality goods and services, airlines, cruise lines and educational institutions. Sed posuere consectetur est at lobortis. Maecenas sed diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+						</p>
+						<p>
+							Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas faucibus mollis interdum. Cras mattis consectetur purus sit amet fermentum.
+						</p>
+						<img src="./_assets_/images/inner-img.jpg" alt="Image of a historical monument">
+						<p>
+							Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis.Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis.Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis.
+						</p>
+						<p>
+							Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.
+						</p>
+						<p>
+							Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+						</p>
+					</div><!-- /#post -->
+				</article><!-- /#entry -->
+			</div><!-- /.col-md-8 -->
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+
+	<section id="footer-links">
+		<img src="./_assets_/images/banner.jpg" alt="Banner" id="banner">
+		<div id="contact">
+			<a href="./"><img src="./_assets_/images/footer-logo.png" alt="Logo" id="footer-logo"></a>
+			<p><span>phone</span> (787) 758-8001</p>
+			<p><span>fax</span> (787) 758-8091</p>
+		</div><!-- /#contact -->
+		<ul id="footer-nav">
+			<li><a href="./">promotional tools</a></li>
+			<li><a href="./">post hurricane info</a></li>
+			<li><a href="./">educational fund</a></li>
+			<li><a href="./">contact</a></li>
+		</ul>
+	</section><!-- /#footer-links -->
+</main>
+
+<footer>
+	<div class="container">
+		<div id="footer-left">
+			<div>&copy; Copyright 2018 Puerto Rico Hotel & Tourism Association</div> <div>33 Resoluci<span style="text-transform: lowercase;">&Oacute;</span>n STE 701-B San Juan, Puerto Rico 00920-2707</div>
+		</div><!-- /#footer-left -->
+		<div id="footer-right">
+			<span>Powered by <a href="https://www.revize.com/" id="revize-logo">revize.</a>, The Government Website Experts</span> <a href="./">login</a> 
+		</div> <!-- /#footer-right -->
+	</div>
+</footer>
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
 
 <script src="_assets_/js/jquery.min.js"></script>
 <script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
-<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
 <script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
 <script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
-<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
-<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
-<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
-<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
-<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
-<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
 <script src="_assets_/js/scripts.js"></script>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5113ea471e44f9dd"></script>
-<script type="text/javascript">
-  addthis.layers({
-    'theme' : 'transparent',
-    'share' : {
-      'position' : 'right',
-      'numPreferredServices' : 6
-    } 
-  });
-</script>
+
 </body>
 </html>
